30af9de29a8634220a218dd9ae347b92cf1aa321
Guilherme Schardong <guilherme.schardong@isr.uc.pt>
2025-10-23 13:49:27 +0100

    Major website update. See below.
    * Style changes
    Using an Edward Tufte inspired CSS by Dave Liepmann (see
    https://edwardtufte.github.io/tufte-css/) with minor changes, such as
    section heading fonts and page content width.
    
    * Page restructuring
    Moved the org files to its own folder and adjusted the build
    rules. Moving all research item related information to their own
    files, thus having more freedom do to add/remove more things without
    changing the page itself.

--- build-site.el
+++ build-site.el
@@ -1,28 +1,46 @@
 ;; Load the publishing system
 (require 'ox-publish)
 
-(setq org-html-validation-link nil            ;; Don't show validation link
-      org-html-head-include-scripts nil       ;; Use our own scripts
+(setq org-html-validation-link nil             ;; Don't show validation link
+      org-html-head-include-scripts nil        ;; Use our own scripts
       org-html-head-include-default-style nil) ;; Use our own styles
-      ;;org-html-head "<link rel=\"stylesheet\" href=\"css/stylesheet.css\" />")
-      ;;org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
       (list
-       (list "schardong.github.io"
-             :recursive t
-             :base-directory "./"
-             :publishing-directory "./"
+       ;; --- Component 1: The Papers/Posts ---
+       (list "papers"
+             :recursive nil
+             :base-directory "org/papers/"
+             :base-extension "org"
+             :publishing-directory "public_html/papers/"
+             :publishing-function 'org-html-publish-to-html
+             :with-author t
+             :with-date nil)
+
+       ;; --- Component 2: The Index Page ---
+       (list "index"
+             :recursive nil
+             :base-directory "org/"
+             :base-extension "org"
+             :publishing-directory "public_html/"
              :publishing-function 'org-html-publish-to-html
              :with-author nil
              :with-creator t
              :with-toc nil
              :section-numbers nil
-             :time-stamp-file t)))
+             :time-stamp-file t)
 
+       ;; --- Component 3: Style files (CSS) ---
+       (list "css"
+             :base-directory "css/"
+             :base-extension "css\\|eot\\|svg\\|ttf\\|woff"
+             :publishing-directory "public_html/css"
+             :publishing-function 'org-publish-attachment
+             :recursive t)
+
+        (list "schardong.github.io" :components '("css" "papers" "index"))))
 
 ;; Generate the site output
 (org-publish-all t)
-
 (message "Build complete!")
